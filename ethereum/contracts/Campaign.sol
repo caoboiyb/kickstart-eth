3cb07229c0620d63f997fef14b62df4fa6c754d2
Nguyen Trung Duc <ngtrungducyb@gmail.com>
2018-09-11 15:21:00 +0700

    fix contract

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -42,9 +42,10 @@ contract Campaign {
     
     function contribute() public payable {
         require(msg.value > minimumContribution);
-        
-        approvers[msg.sender] = true;
-        approversCount++;
+        if (approvers[msg.sender] != true) {
+            approvers[msg.sender] = true;
+            approversCount++;
+        }
     }
     
     function createRequest(string description, uint value, address recipient) public restricted {
